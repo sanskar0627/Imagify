08681b78155790758583d37e36d81d0e549628cd
Sanskar Shukla <sanskar0627@gmail.com>
2025-07-07 23:57:09 +0530

    Completed Frontend Now BackEND STarted

--- client/src/pages/BuyCredit.jsx
+++ client/src/pages/BuyCredit.jsx
@@ -1,11 +1,18 @@
 import React, { useContext } from 'react'
 import { assets, plans } from '../assets/assets'
 import { AppContext } from '../context/AppContext'
+import { motion } from "framer-motion";
 
 const BuyCredit = () => {
   const { user } = useContext(AppContext)
   return (
-    <div className='min-h-[80vh] text-center pt-14 mb-10'>
+    <motion.div
+      initial={{ opacity: 0.2, y: 100 }}
+      transition={{ duration: 1 }}
+      whileInView={{ opacity: 1, y: 0 }}
+      viewport={{ once: true }}
+
+      className='min-h-[80vh] text-center pt-14 mb-10'>
       <button className='border border-gray-400 px-10 py-2 rounded-full mb-6'>Our Plans</button>
       <h1 className='text-center text-3xl font-medium mb-6 sm:mb-10'>Choose the plan</h1>
 
@@ -19,7 +26,7 @@ const BuyCredit = () => {
               <span className='text-3xl'>
                 ${item.price} </span>/ {item.credits} credits</p>
             <button className='w-full bg-gray-800 text-white mt-8 text-sm rounded-md py-2.5 min-w-52'>
-              {user ? 'Purchase':' Get Started'}
+              {user ? 'Purchase' : ' Get Started'}
             </button>
           </div>
         ))}
@@ -27,7 +34,7 @@ const BuyCredit = () => {
 
 
 
-    </div>
+    </ motion.div>
 
   )
 }
