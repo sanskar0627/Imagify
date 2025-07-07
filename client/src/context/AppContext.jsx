4714337861f413cf64f9aa5fdfb485bd5fecf8c7
Sanskar Shukla <sanskar0627@gmail.com>
2025-07-07 22:14:59 +0530

    COmpleted ANimation

--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -3,16 +3,17 @@ import { createContext, useState } from "react";
 export const AppContext = createContext();
 
 const AppContextProvider = (props) => {
-   const [user, setuser] = useState(null);
-   const [showLogin, setshowLogin] = useState(false);
+  const [user, setuser] = useState(null);
+  const [showLogin, setshowLogin] = useState(false);
 
-   const value = {
-      user, setuser, showLogin, setshowLogin
-   }
-   return (
-      <AppContext.Provider value={value}>
-         {props.children}
-      </AppContext.Provider>
-   )
-}
-export default AppContextProvider
+  const value = {
+    user,
+    setuser,
+    showLogin,
+    setshowLogin,
+  };
+  return (
+    <AppContext.Provider value={value}>{props.children}</AppContext.Provider>
+  );
+};
+export default AppContextProvider;
