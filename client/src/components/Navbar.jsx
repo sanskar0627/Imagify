f616f10f07df5ad985459e8ff7efea57ea09fa06
Sanskar Shukla <sanskar0627@gmail.com>
2025-07-07 21:32:09 +0530

    Completed The login ANd SigUp PAge

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -4,7 +4,7 @@ import { Link, useNavigate } from 'react-router-dom'
 import { AppContext } from '../context/AppContext'
 
 const Navbar = () => {
-    const { user } = useContext(AppContext)
+    const { user ,setshowLogin} = useContext(AppContext)
     const navigate = useNavigate();
     return (
         <div className='flex items-center justify-between py-4'>
@@ -34,7 +34,7 @@ const Navbar = () => {
                     : // if user is not logged in then this div is shwoing
                     <div className='flex items-center gap-2 sm:gap-5'>
                         <p onClick={() => navigate('/buy')} className='cursor-pointer'>Pricing</p>
-                        <button className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
+                        <button  onClick={()=>setshowLogin(true)} className='bg-zinc-800 text-white px-7 py-2 sm:px-10 text-sm rounded-full'>Login</button>
                     </div>
                 }
             </div>
--- client/src/context/AppContext.jsx
+++ client/src/context/AppContext.jsx
@@ -1,17 +1,18 @@
 import { createContext, useState } from "react";
 
-export const AppContext=createContext();
+export const AppContext = createContext();
 
-const AppContextProvider=(props)=>{
-     const [user, setuser] = useState(null);
+const AppContextProvider = (props) => {
+   const [user, setuser] = useState(null);
+   const [showLogin, setshowLogin] = useState(false);
 
-     const value={
-        user,setuser
-     }
-     return(
-        <AppContext.Provider value={value}>
-            {props.children}
-        </AppContext.Provider>
-     )
+   const value = {
+      user, setuser, showLogin, setshowLogin
+   }
+   return (
+      <AppContext.Provider value={value}>
+         {props.children}
+      </AppContext.Provider>
+   )
 }
 export default AppContextProvider
