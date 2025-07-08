2dab08cb459c1cf24bc0279f9ce2f5e81c91f437
Sanskar Shukla <sanskar0627@gmail.com>
2025-07-08 10:45:26 +0530

    Working on MongoDB

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -1,41 +1,54 @@
 import userModel from "../models/userModel.js";
-import bcrypt from 'bcrypt';
-import jwt from 'jsonwebtoken';
+import bcrypt from "bcrypt";
+import jwt from "jsonwebtoken";
 
 const registerUser = async (req, res) => {
     try {
         const { name, email, password } = req.body;
 
         if (!name || !email || !password) {
-            return res.json({ sucess: false, message: 'Missing Details' });
+            return res.json({ sucess: false, message: "Missing Details" });
         }
-        const salt = await bcrypt.genSalt(10)
-        const hashedPassword = await bcrypt.hash(password, salt)
+        const salt = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, salt);
 
         const userData = {
             name,
             email,
-            password: hashedPassword
-        }
+            password: hashedPassword,
+        };
 
-        const newUser = new userModel(userData)
-        const user = await newUser.save()
+        const newUser = new userModel(userData);
+        const user = await newUser.save();
 
-        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
+        const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET);
         res.json({
             success: true,
             token,
-            user: { name: user.name }
-        })
-
-
+            user: { name: user.name },
+        });
     } catch (error) {
         console.log(error);
         res.json({
             success: false,
-            message:error.message
-        })
+            message: error.message,
+        });
     }
 };
 
+const userCredits = async (req, res) => {
+    try {
+        const { userId } = req.body
+        const user = await userModel.findById(userId)
+        res.json({ success: true, credits: user.creditBalance, user: { name: user.name } })
+    } catch (error) {
+        console.log(error.message)
+        res.json({ success: false, message: error.message })
+    }
+}
+
+
+
+
+export { registerUser, loginUser };
 
--- server/server.js
+++ server/server.js
@@ -2,6 +2,7 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './config/mongodb.js'
+import userRouter from './routes/userRoutes.js'
 
 const PORT = process.env.PORT || 4000
 const app = express()
@@ -10,6 +11,7 @@ app.use(express.json())
 app.use(cors())
 await connectDB()
 
+app.use('/api/user',userRouter)
 app.get('/', (req, res) => res.send("API Working"))
 
 app.listen(PORT, () => console.log('Server running on port ' + PORT));
