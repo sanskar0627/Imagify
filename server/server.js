34fc2bb56315043414a8b98187c8900713372669
Sanskar Shukla <sanskar0627@gmail.com>
2025-07-09 17:48:31 +0530

    chnages

--- server/server.js
+++ server/server.js
@@ -16,7 +16,7 @@ app.use(cors({
   ],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
+  allowedHeaders: ['Content-Type', 'Authorization', 'token']
 }));
 await connectDB()
 
